a0f910628f7ad14b03e67e9eb12514aad84228ff
Cameron Cousins <cameroncousins18@gmail.com>
2020-08-30 21:02:57 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -14,6 +14,45 @@
 #   ask for input again until you get a valid input
 # end
 
+# def display_board(board)
+#   puts " #{board[0]} | #{board[1]} | #{board[2]} "
+#   puts "-----------"
+#   puts " #{board[3]} | #{board[4]} | #{board[5]} "
+#   puts "-----------"
+#   puts " #{board[6]} | #{board[7]} | #{board[8]} "
+# end
+# 
+# def input_to_index(input)
+#   input.to_i - 1
+# end 
+# 
+# def move(board, position, character="X")
+#   board[position] = character
+# end 
+# 
+# def valid_move?(board, index) 
+#   # Present on the game board and not already filled with a token.
+#   if index.between?(0,8) && !position_taken?(board, index) 
+#   # returns true if the move is valid and false or nil if not
+#     true 
+#   else 
+#   # returns nil or false for a position that is not on the board (like 100)
+#     false 
+#   end
+# end 
+# 
+# def position_taken?(board, index)
+#   if board[index] == ' ' || board[index] == '' || board[index] == nil
+#     return false 
+#   else 
+#     return true 
+#   end 
+# end 
+# 
+# def turn(board)
+#   puts "Please enter 1-9:"
+# end
+
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -22,33 +61,30 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def input_to_index(input)
-  input.to_i - 1
-end 
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
 
-def move(board, position, character="X")
-  board[position] = character
-end 
+def move(board, index, current_player = "X")
+  board[index] = current_player
+end
 
-def valid_move?(board, index) 
-  # Present on the game board and not already filled with a token.
-  if index.between?(0,8) && !position_taken?(board, index) 
-  # returns true if the move is valid and false or nil if not
-    true 
-  else 
-  # returns nil or false for a position that is not on the board (like 100)
-    false 
-  end
-end 
+def position_taken?(board, location)
+  board[location] != " " && board[location] != ""
+end
 
-def position_taken?(board, index)
-  if board[index] == ' ' || board[index] == '' || board[index] == nil
-    return false 
-  else 
-    return true 
-  end 
-end 
+def valid_move?(board, index)
+  index.between?(0,8) && !position_taken?(board, index)
+end
 
 def turn(board)
   puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move?(board, index)
+    move(board, index)
+    display_board(board)
+  else
+    turn(board)
+  end
 end
